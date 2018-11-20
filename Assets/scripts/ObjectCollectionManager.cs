ff71908ae89256e6ff93a99a6d6055cb6fd708b5
Raminka <ramina.suleimanova@telecom-sudparis.eu>
2018-11-20 14:13:29 +0100

    init

---
+++ Assets/scripts/ObjectCollectionManager.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using HoloToolkit.Unity;
+using UnityEngine;
+
+public class ObjectCollectionManager : Singleton<ObjectCollectionManager>
+{
+
+    [Tooltip("A collection of Wide building prefabs to generate in the world.")]
+    public List<GameObject> CubePrefabs;
+
+    [Tooltip("The desired size of wide buildings in the world.")]
+    public Vector3 SquareSize = new Vector3(.5f, .5f, .5f);
+
+    [Tooltip("The ground to place under buildings.")]
+    public GameObject BuildingGround;
+
+    [Tooltip("Will be calculated at runtime if is not preset.")]
+    public float ScaleFactor;
+
+    public List<GameObject> ActiveHolograms = new List<GameObject>();
+
+    public void CreateSquare(int number, Vector3 positionCenter, Quaternion rotation)
+    {
+        CreateBuilding(CubePrefabs[number], positionCenter, rotation, SquareSize);
+    }
+
+    private void CreateBuilding(GameObject buildingToCreate, Vector3 positionCenter, Quaternion rotation, Vector3 desiredSize)
+    {
+        // Stay center in the square but move down to the ground
+        var position = positionCenter ; //new Vector3(0, desiredSize.y * .5f, 0);
+
+        GameObject newObject = Instantiate(buildingToCreate, position, rotation) as GameObject;
+
+        if (newObject != null)
+        {
+            // Set the parent of the new object the GameObject it was placed on
+            newObject.transform.parent = gameObject.transform;
+
+            newObject.transform.localScale = RescaleToSameScaleFactor(buildingToCreate);
+            ActiveHolograms.Add(newObject);
+        }
+    }
+
+    public void CreateGround(GameObject groundToCreate, Vector3 positionCenter, Quaternion rotation, Vector3 size)
+    {
+        // Stay center in the square but move down to the ground
+        var position = positionCenter - new Vector3(0, size.y * .5f, 0);
+
+        GameObject newObject = Instantiate(groundToCreate, position, rotation) as GameObject;
+
+        if (newObject != null)
+        {
+            // Set the parent of the new object the GameObject it was placed on
+            newObject.transform.parent = gameObject.transform;
+
+            newObject.transform.localScale = StretchToFit(groundToCreate, size);
+            ActiveHolograms.Add(newObject);
+        }
+    }
+
+    private Vector3 RescaleToSameScaleFactor(GameObject objectToScale)
+    {
+        // ReSharper disable once CompareOfFloatsByEqualityOperator
+        if (ScaleFactor == 0f)
+        {
+            CalculateScaleFactor();
+        }
+
+        return objectToScale.transform.localScale * ScaleFactor;
+    }
+
+    private Vector3 StretchToFit(GameObject obj, Vector3 desiredSize)
+    {
+        var curBounds = GetBoundsForAllChildren(obj).size;
+
+        return new Vector3(desiredSize.x / curBounds.x / 2, desiredSize.y, desiredSize.z / curBounds.z / 2);
+    }
+
+    private void CalculateScaleFactor()
+    {
+        float maxScale = float.MaxValue;
+
+        var ratio = CalcScaleFactorHelper(CubePrefabs, SquareSize);
+        if (ratio < maxScale)
+        {
+            maxScale = ratio;
+        }
+
+        ScaleFactor = maxScale;
+    }
+
+    private float CalcScaleFactorHelper(List<GameObject> objects, Vector3 desiredSize)
+    {
+        float maxScale = float.MaxValue;
+
+        foreach (var obj in objects)
+        {
+            var curBounds = GetBoundsForAllChildren(obj).size;
+            var difference = curBounds - desiredSize;
+
+            float ratio;
+
+            if (difference.x > difference.y && difference.x > difference.z)
+            {
+                ratio = desiredSize.x / curBounds.x;
+            }
+            else if (difference.y > difference.x && difference.y > difference.z)
+            {
+                ratio = desiredSize.y / curBounds.y;
+            }
+            else
+            {
+                ratio = desiredSize.z / curBounds.z;
+            }
+
+            if (ratio < maxScale)
+            {
+                maxScale = ratio;
+            }
+        }
+
+        return maxScale;
+    }
+
+    private Bounds GetBoundsForAllChildren(GameObject findMyBounds)
+    {
+        Bounds result = new Bounds(Vector3.zero, Vector3.zero);
+
+        foreach (var curRenderer in findMyBounds.GetComponentsInChildren<Renderer>())
+        {
+            if (result.extents == Vector3.zero)
+            {
+                result = curRenderer.bounds;
+            }
+            else
+            {
+                result.Encapsulate(curRenderer.bounds);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ Assets/scripts/ObjectPlacer.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using HoloToolkit.Unity;
+using UnityEngine;
+
+public class ObjectPlacer : MonoBehaviour
+{
+    /**
+     * Comme son nom indique ce sont des boîtes de debug. L'objet est centré dans ces boîtes.
+     * Et ce sont ces boîtes là qui sont en contact direct avec les autres objets (murs/sol).
+     * Si l'objet semble d'être mal placé, passez DrawDebugBoxes en true.
+     **/ 
+    public bool DrawDebugBoxes = false;
+
+    /**
+     * Permet de dessiner les halogrammes. Sa valeur est toujeour TRUE 
+     * sauf si on veut travailler qu'avec les DebugBoxes pour les positions précis
+     **/ 
+    public bool DrawObjects = true;
+
+
+    public SpatialUnderstandingCustomMesh SpatialUnderstandingMesh;
+
+    private readonly List<BoxDrawer.Box> _lineBoxList = new List<BoxDrawer.Box>();
+
+    private readonly Queue<PlacementResult> _results = new Queue<PlacementResult>();
+
+    private bool _timeToHideMesh;
+    private BoxDrawer _boxDrawing;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (DrawDebugBoxes)
+        {
+            /**
+             * Tracé des boîtes de debug
+             **/ 
+            _boxDrawing = new BoxDrawer(gameObject);
+        }
+
+    }
+
+    void Update()
+    {
+        ProcessPlacementResults();
+
+        if (_timeToHideMesh)
+        {
+            SpatialUnderstandingState.Instance.HideText = true;
+            HideGridEnableOcclulsion();
+            _timeToHideMesh = false;
+        }
+
+        if (DrawDebugBoxes)
+        {
+            _boxDrawing.UpdateBoxes(_lineBoxList);
+        }
+
+    }
+
+    private void HideGridEnableOcclulsion()
+    {
+        SpatialUnderstandingMesh.DrawProcessedMesh = false;
+    }
+
+    public void CreateScene()
+    {
+        // Only if we're enabled
+        if (!SpatialUnderstanding.Instance.AllowSpatialUnderstanding)
+        {
+            return;
+        }
+
+        SpatialUnderstandingDllObjectPlacement.Solver_Init();
+
+        SpatialUnderstandingState.Instance.SpaceQueryDescription = "Generating World";
+
+        List<PlacementQuery> queries = new List<PlacementQuery>();
+
+        if (DrawObjects)
+        {
+            queries.AddRange(AddObjects());
+        }
+        
+        GetLocationsFromSolver(queries);
+    }
+
+    /**
+     * 
+     * 
+     **/ 
+    public List<PlacementQuery> AddObjects()
+    {
+
+        var queries = CreateLocationQueriesForSolver(ObjectCollectionManager.Instance.CubePrefabs.Count, ObjectCollectionManager.Instance.SquareSize, ObjectType.Square);
+        return queries;
+    }
+
+   
+
+    private int _placedSquareBuilding;
+
+    private void ProcessPlacementResults()
+    {
+        if (_results.Count > 0)
+        {
+            var toPlace = _results.Dequeue();
+            // Output
+            if (DrawDebugBoxes)
+            {
+                DrawBox(toPlace, Color.red);
+            }
+            var rotation = Quaternion.LookRotation(toPlace.Normal, Vector3.up);
+
+            switch (toPlace.ObjType)
+            {
+                case ObjectType.Square:
+                    ObjectCollectionManager.Instance.CreateSquare(_placedSquareBuilding++, toPlace.Position, rotation);
+                    break;
+            }
+        }
+    }
+
+    private void DrawBox(PlacementResult boxLocation, Color color)
+    {
+        if (boxLocation != null)
+        {
+            _lineBoxList.Add(
+                new BoxDrawer.Box(
+                    boxLocation.Position,
+                    Quaternion.LookRotation(boxLocation.Normal, Vector3.up),
+                    color,
+                    boxLocation.Dimensions * 0.5f)
+            );
+        }
+    }
+
+    private void GetLocationsFromSolver(List<PlacementQuery> placementQueries)
+    {
+#if UNITY_WSA && !UNITY_EDITOR
+        System.Threading.Tasks.Task.Run(() =>
+        {
+            // Go through the queries in the list
+            for (int i = 0; i < placementQueries.Count; ++i)
+            {
+                var result = PlaceObject(placementQueries[i].ObjType.ToString() + i,
+                                         placementQueries[i].PlacementDefinition,
+                                         placementQueries[i].Dimensions,
+                                         placementQueries[i].ObjType,
+                                         placementQueries[i].PlacementRules,
+                                         placementQueries[i].PlacementConstraints);
+                if (result != null)
+                {
+                    _results.Enqueue(result);
+                }
+            }
+
+            _timeToHideMesh = true;
+        });
+#else
+        _timeToHideMesh = true;
+#endif
+    }
+
+    private PlacementResult PlaceObject(string placementName,
+        SpatialUnderstandingDllObjectPlacement.ObjectPlacementDefinition placementDefinition,
+        Vector3 boxFullDims,
+        ObjectType objType,
+        List<SpatialUnderstandingDllObjectPlacement.ObjectPlacementRule> placementRules = null,
+        List<SpatialUnderstandingDllObjectPlacement.ObjectPlacementConstraint> placementConstraints = null)
+    {
+
+        // New query
+        if (SpatialUnderstandingDllObjectPlacement.Solver_PlaceObject(
+                placementName,
+                SpatialUnderstanding.Instance.UnderstandingDLL.PinObject(placementDefinition),
+                (placementRules != null) ? placementRules.Count : 0,
+                ((placementRules != null) && (placementRules.Count > 0)) ? SpatialUnderstanding.Instance.UnderstandingDLL.PinObject(placementRules.ToArray()) : IntPtr.Zero,
+                (placementConstraints != null) ? placementConstraints.Count : 0,
+                ((placementConstraints != null) && (placementConstraints.Count > 0)) ? SpatialUnderstanding.Instance.UnderstandingDLL.PinObject(placementConstraints.ToArray()) : IntPtr.Zero,
+                SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticObjectPlacementResultPtr()) > 0)
+        {
+            SpatialUnderstandingDllObjectPlacement.ObjectPlacementResult placementResult = SpatialUnderstanding.Instance.UnderstandingDLL.GetStaticObjectPlacementResult();
+
+            return new PlacementResult(placementResult.Clone() as SpatialUnderstandingDllObjectPlacement.ObjectPlacementResult, boxFullDims, objType);
+        }
+
+        return null;
+    }
+
+    private List<PlacementQuery> CreateLocationQueriesForSolver(int desiredLocationCount, Vector3 boxFullDims, ObjectType objType)
+    {
+        List<PlacementQuery> placementQueries = new List<PlacementQuery>();
+
+        var halfBoxDims = boxFullDims * .5f;
+
+
+        /**
+         * variable permet de calculer la distance entre les objets
+         **/ 
+        var disctanceFromOtherObjects = halfBoxDims.x > halfBoxDims.z ? halfBoxDims.x * 3f : halfBoxDims.z * 3f;
+
+        for (int i = 0; i < desiredLocationCount; ++i)
+        {
+            /**
+             * Les regles de placemnt d'objet. La regle definit que l'objet doit être éloigné d'un autre objet ou point ou autre...
+             * Variable à exploiter.
+             **/ 
+            var placementRules = new List<SpatialUnderstandingDllObjectPlacement.ObjectPlacementRule>
+            {
+                SpatialUnderstandingDllObjectPlacement.ObjectPlacementRule.Create_AwayFromOtherObjects(disctanceFromOtherObjects)
+            };
+
+
+            /**
+             * Les contraintes de placement des objets. On peut placer un objet près d'un centre ou près d'un point à particulier.
+             * Cette variable est aussi à exploiter par rapport ce qu'on veux voir
+             **/
+           // var placementConstraints = new List<SpatialUnderstandingDllObjectPlacement.ObjectPlacementConstraint>(); //->par défaut
+            var placementConstraints = new List<SpatialUnderstandingDllObjectPlacement.ObjectPlacementConstraint> {
+                SpatialUnderstandingDllObjectPlacement.ObjectPlacementConstraint.Create_NearCenter(),
+            };
+
+            /**
+             * Ici nous définissons où veut on placer les objets.
+             * Nous utilisons pour le moment que deux création d'objet : Create_OnFloor et Create_OnWall.
+             * On peut exploirer plus cette variable pour mieux définir la position des objets.
+             **/
+            SpatialUnderstandingDllObjectPlacement.ObjectPlacementDefinition placementDefinition = SpatialUnderstandingDllObjectPlacement.ObjectPlacementDefinition.Create_OnWall(halfBoxDims, 1.2f, 1.9f);//Create_OnFloor(halfBoxDims);
+
+            placementQueries.Add(
+                new PlacementQuery(placementDefinition,
+                    boxFullDims,
+                    objType,
+                    placementRules,
+                    placementConstraints
+                ));
+        }
+
+        return placementQueries;
+    }
+
+}
