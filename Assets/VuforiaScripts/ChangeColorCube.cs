4da7193f33d48f4212a4fc5205b793487aa50dc7
Aymeric Gleye <gleye.a@gmail.com>
2018-11-22 13:47:42 +0100

    Test Vuforia précision

---
+++ Assets/VuforiaScripts/ChangeColorCube.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ChangeColorCube : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+    public GameObject myCube;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED
+            //|| newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED
+            )
+        {
+            myCube.GetComponent<Renderer>().material.color = Color.green;
+            Debug.Log("Image found, changed color of cube");
+        }
+        else
+        {
+            myCube.GetComponent<Renderer>().material.color = Color.red;
+            Debug.Log("Image lost, changed color of cube");
+        }
+    }
+}
---
+++ Assets/VuforiaScripts/PopBoxes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class PopBoxes : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+    public GameObject Cubes;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED
+            //|| newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED
+            )
+        {
+            Cubes.SetActive(true);
+            Debug.Log("Image found, changed color of cube");
+        }
+        else
+        {
+            Cubes.SetActive(false);
+            Debug.Log("Image lost, changed color of cube");
+        }
+    }
+}
