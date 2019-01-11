c2219af953ab656070b5a88648b1c44e3b2c2a77
Thanaell <gaelle_clavelin@orange.fr>
2019-01-11 15:14:08 +0100

    on récupère la distance parcourue pendant une session et on la stocke dans un fichier .txt

---
+++ Assets/scripts/WalkedDistance.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+
+public class WalkedDistance : MonoBehaviour {
+    private float oldX;
+    private float newX;
+    private float oldZ;
+    private float newZ;
+    private float timer;
+    public float walkedDistance;
+
+  
+	// Use this for initialization
+	void Start () {
+        oldX = transform.position.x;
+        oldZ = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if( timer >= 0.5)
+        {
+            timer = 0;
+            walkedDistance += Mathf.Sqrt((oldX - newX) * (oldX - newX) + (oldZ - newZ) * (oldZ - newZ));
+            Debug.Log(walkedDistance);
+            oldZ = newZ;
+            oldX = newX;
+            newZ = transform.position.x;
+            newX = transform.position.z;
+
+            //write walkedDistance to file in System/File Explorer/LocalAppData/CPARRoW/LocalState
+            string path = Path.Combine(Application.persistentDataPath, "MyFile.txt");
+            File.WriteAllText(path, "Total walked distance in last session : "+ walkedDistance.ToString());
+        }
+        timer += Time.deltaTime;	
+	}
+}
