87134b8fd0abef873cb32e032a500cb70da18515
Raminka <ramina.suleimanova@telecom-sudparis.eu>
2019-01-14 14:08:21 +0100

    audio manager
    add sound to objects

---
+++ Assets/scripts/Holograms/Placement/AudioManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioSource audioData;
+    public AudioClip audio;
+
+
+    // Use this for initialization
+    void Start () {
+        audioData.PlayOneShot(audio, 1);
+;	}
+
+}
--- Assets/scripts/Holograms/Placement/CustomObjectScript.cs
+++ Assets/scripts/Holograms/Placement/CustomObjectScript.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class CustomObjectScript : MonoBehaviour {
 
+
+    public AudioClip audio;
+
     public void addComponents(GameObject go)
     {
         go.AddComponent<InteractionScript>();
+        go.AddComponent<AudioSource>();
+        go.AddComponent<AudioManager>();
+        go.GetComponent<AudioSource>().maxDistance = 2;
+        go.GetComponent<AudioSource>().minDistance = 0;
+        go.GetComponent<AudioManager>().audioData = go.GetComponent<AudioSource>();
+        go.GetComponent<AudioManager>().audio = audio;
     }
 }
