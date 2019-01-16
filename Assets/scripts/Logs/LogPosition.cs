949bb6c3e3646b711fce2fef57a94f0b49b3ee26
Thanaell <gaelle_clavelin@orange.fr>
2019-01-16 12:29:25 +0100

    log manager et logs de position

---
+++ Assets/scripts/Logs/LogPosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LogPosition : MonoBehaviour {
+    //temps d'échantillonnage à définir dans l'inspecteur
+    public float tempsEchantillon;
+    private string path;
+    private float x;
+    private float y;
+    private float z;
+    private float timer=0;
+    private float globalTimer=0;
+
+    void Start () {
+        path = Path.Combine(Application.persistentDataPath, "LogPosition.txt");
+        File.WriteAllText(path,"");
+    }
+	
+	void Update () {
+        if (timer >= tempsEchantillon)
+        {
+            timer = 0;
+            x = Camera.main.transform.position.x;
+            y = Camera.main.transform.position.y;
+            z = Camera.main.transform.position.z;
+
+            File.AppendAllText(path, globalTimer.ToString() + ";" + x + ";" + y + ";" + z + System.Environment.NewLine);
+        }
+        timer += Time.deltaTime;
+        globalTimer += Time.deltaTime;
+    }
+}
---
+++ Assets/scripts/Logs/WalkedDistance.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class WalkedDistance : MonoBehaviour
+{
+
+    public float tempsEchantillon;
+
+    private float oldX;
+    private float newX;
+    private float oldZ;
+    private float newZ;
+    private float timer=0;
+    private float globalTimer=0;
+    private float walkedDistance=0;
+
+    private string path;
+
+    //getter de walked distance
+    public float getWalkedDistance()
+    {
+        return walkedDistance;
+    }
+   
+
+    void Start()
+    {
+        oldX = Camera.main.transform.position.x;
+        oldZ = Camera.main.transform.position.z;
+
+        path = Path.Combine(Application.persistentDataPath, "WalkedDistance.txt");
+        File.WriteAllText(path, "0;0" + System.Environment.NewLine);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= tempsEchantillon)
+        {
+            timer = 0;
+            walkedDistance += Mathf.Sqrt((oldX - newX) * (oldX - newX) + (oldZ - newZ) * (oldZ - newZ));
+
+            oldZ = newZ;
+            oldX = newX;
+            newZ = Camera.main.transform.position.x;
+            newX = Camera.main.transform.position.z;
+
+            File.AppendAllText(path, globalTimer.ToString() + ";" + walkedDistance.ToString() + System.Environment.NewLine);
+        }
+        timer += Time.deltaTime;
+        globalTimer += Time.deltaTime;
+    }
+
+
+    //permet d'écrire dans le fichier walkedDistance la vitesse moyenne depuis le début de la session quand on quitte l'application
+
+    private void OnApplicationQuit()
+    {
+        File.AppendAllText(path, "Vitesse moyenne au bout de " + globalTimer.ToString() + " : " + walkedDistance / globalTimer + " m/s");
+    }
+}
