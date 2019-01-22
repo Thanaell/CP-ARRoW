0c2a299a5e52406a10c475c896fcb5bf064b59e8
gleyea <gleye.a@gmail.com>
2019-01-22 15:41:06 +0100

    Ajout Anchor objets

---
+++ Assets/scripts/AddAnchor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.SpatialMapping;
+
+public class AddAnchor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.parent.gameObject.AddComponent<Interpolator>();
+        transform.parent.gameObject.AddComponent<TapToPlace>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
