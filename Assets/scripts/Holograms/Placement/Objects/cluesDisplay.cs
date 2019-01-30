e5e21257114a3e29b8cd7dad3a47f17c9b901317
Raminka <ramina.suleimanova@telecom-sudparis.eu>
2019-01-30 09:54:03 +0100

    update clueDisplay script

--- Assets/scripts/Holograms/Placement/Objects/cluesDisplay.cs
+++ Assets/scripts/Holograms/Placement/Objects/cluesDisplay.cs
@@ -13,22 +13,17 @@ public class cluesDisplay : MonoBehaviour {
      * Cette variable indique le nombre de clés déjà ramasser et connu par le coffre
      */ 
     int clueTaken = 0;
-    
-	void Update () {
+
+    private void Start()
+    {
         /*
-         * Si le joueur a vu le coffre et que les clés ne sont pas encore collectées on affiche les objets à
-         * collecter au-dessus du coffre
-         */ 
-        if (clueTaken == 0 & ObjectCollectionManager.Instance.TreasureIsSeen)
-        {
-            /*
-             * On instancie les copies des objets à ramasser 
-             */ 
-            for (int i = 0; i < ObjectCollectionManager.Instance.WallPrefabs.Count; i++)
+         * On instancie les copies des objets à ramasser. Les objets sont créés mais affichés au-dessus du coffre
+         */
+        for (int i = 0; i < ObjectCollectionManager.Instance.WallPrefabs.Count; i++)
             {
-                GameObject newObject = Instantiate(ObjectCollectionManager.Instance.WallPrefabs[i], 
-                    gameObject.transform.position + new Vector3(0, 0.4f, 0) + gameObject.transform.parent.transform.parent.transform.right * (-ObjectCollectionManager.Instance.WallPrefabs.Count * 0.025f + i * 0.05f), 
-                    transform.parent.transform.parent.rotation * Quaternion.Euler(90f * ObjectCollectionManager.Instance.WallPrefabs[i].transform.forward), 
+                GameObject newObject = Instantiate(ObjectCollectionManager.Instance.WallPrefabs[i],
+                    gameObject.transform.position + new Vector3(0, 0.4f, 0) + gameObject.transform.parent.transform.parent.transform.right * (-ObjectCollectionManager.Instance.WallPrefabs.Count * 0.025f + i * 0.05f),
+                    transform.parent.transform.parent.rotation * Quaternion.Euler(90f * ObjectCollectionManager.Instance.WallPrefabs[i].transform.forward),
                     gameObject.transform) as GameObject;
                 newObject.transform.SetSiblingIndex(i);
                 /*Pour ne pas causer les soucis de collision avec le cursor, nous désactivons les colliders*/
@@ -37,8 +32,11 @@ public class cluesDisplay : MonoBehaviour {
                     newObject.GetComponent<Collider>().enabled = false;
                 }
             }
-        }
+ 
 
+    }
+
+    void Update () {
         /*
          * Si l'objet collecté enregistré par coffre n'est pas le même que l'objet activé sur la scène 
          * alors on désactive l'affichage de l'objet nécessaire à ramasser et on on change la variable
