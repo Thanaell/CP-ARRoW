4a82bb6ca00a52e44dbb3955160abb7f07d47f32
Raminka <ramina.suleimanova@telecom-sudparis.eu>
2019-01-31 09:59:23 +0100

    clue collected sound

---
+++ Assets/scripts/Holograms/Placement/Objects/SoundClueManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundClueManager : MonoBehaviour {
+
+
+    public AudioClip audioCall;
+    public AudioClip audioClueCollected;
+    public AudioSource audioSource;
+
+    
+    void Start()
+    {
+        audioSource.clip = audioCall;
+        audioSource.loop = true;
+        audioSource.volume = 0.40f;
+        audioSource.Play();
+    }
+
+    void ClueCollected()
+    {
+
+        audioSource.Stop();
+        audioSource.clip = audioClueCollected;
+        audioSource.volume = 0.40f;
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+}
