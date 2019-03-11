c0d01bcd782a529f10c0cb8f1e0938a07c0ea8ef
anelorG <al.guinet@hotmail.fr>
2019-03-11 17:58:26 +0100

    Add SceneMenu avec bouttons qui permettent de passer d'une scène à l'autre, SpatialMapping Scene, No effect scene, SaptialAnchor Scene (empty) ; il reste à ajouter spatialanchor à SpatialAnchor Scene et cofigurer le menu pour qu'il permette de faire passer d'une scène à l'autre ;

---
+++ Assets/scripts/Logs/MenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public void Exit()
+    {
+        
+        Application.Quit();
+    }
+    public void LanceSpatialMapping()
+    
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void LanceSpatialAnchors()
+
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
+    public void LanceNoEffect()
+
+    {
+        SceneManager.LoadScene(3);
+
+    }
+}
